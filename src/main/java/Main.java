e9012349bbd2ef0e363be262889dbf28956f7b4e
ivanluchkin <id.luchkin@gmail.com>
2020-10-29 00:18:07 +0200

    Image groups are now moved in transactions after all photos are processed successfully.
    TODO:
    - retreive INITFOLDER path from cmd args
    - rename the last files from the folder
    - create a Win64 executable command line app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,17 +14,15 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-    /**
-     * upper limit of photos
-     * export valid groups of images in transactions to another folder
-     */
 
     static Path RESFOLDER;
     static Path INITFOLDER;
     static File[] FILES;
+    static final int MAX_CHAIN_SIZE = 7;
+    static Map<File, String> toBeRenamed = new HashMap<>();
 
     public static void main(String...args) {
-        INITFOLDER = Paths.get("/Users/ivanluchkin/Downloads/barcodes");
+        INITFOLDER = Paths.get("/Users/ivanluchkin/Downloads/mcopy");
         File[] files = new File(INITFOLDER.toString()).listFiles();
         if (files == null) {
             System.out.println("No files found in this folder.");
@@ -41,19 +39,16 @@ public class Main {
         File[] copy = Arrays.copyOf(FILES, FILES.length);
         Arrays.sort(copy, Comparator.naturalOrder());
         System.out.println(Arrays.toString(copy));
-        Arrays.sort(FILES, new Comparator<File>() {
-            @Override
-            public int compare(File prev, File next) {
-                String prevName = prev.getName();
-                String nextName = next.getName();
-                String prevID = prevName.substring(prevName.length() - 8, prevName.length() - 4);
-                String nextID = nextName.substring(nextName.length() - 8, nextName.length() - 4);
-                if (prevID.equals("") || nextID.equals("")) return 0;
-                return Integer.parseInt(prevID) - Integer.parseInt(nextID);
-            }
+        Arrays.sort(FILES, (prev, next) -> {
+            String prevName = prev.getName();
+            String nextName = next.getName();
+            String prevID = prevName.substring(prevName.length() - 8, prevName.length() - 4);
+            String nextID = nextName.substring(nextName.length() - 8, nextName.length() - 4);
+            if (prevID.equals("") || nextID.equals("")) return 0;
+            return Integer.parseInt(prevID) - Integer.parseInt(nextID);
         });
         try {
-            RESFOLDER = Paths.get("/Users/ivanluchkin/Downloads/barcodes" + "/res");
+            RESFOLDER = Paths.get(INITFOLDER + "/res");
             Files.createDirectory(RESFOLDER);
         } catch (FileAlreadyExistsException faee) {
             System.out.println("Result folder already exists.");
@@ -61,39 +56,53 @@ public class Main {
         } catch (IOException ioe) {
             System.out.println("Could not create result folder.");
         }
-        showFiles(0, 0,"NO_RES_YET", "");
+        processFiles(0, 0,"NO_RES_YET", "");
     }
 
-    public static void showFiles(int fileIndex, int counter, String lastResult, String prevFileType) {
+    public static void processFiles(int fileIndex, int counter, String lastResult, String prevFileType) {
         String lastBarcodeResult = lastResult;
+        if (counter > MAX_CHAIN_SIZE) {
+            System.out.println("Too many failed scans. Please, review the source folder.");
+            return;
+        }
         if (fileIndex >= FILES.length) return;
         File file = FILES[fileIndex];
         String result = getResult(file);
         if (result.equals("Could not find a barcode on the image.")) {
             switch (prevFileType) {
-                case "CHILD" : file.renameTo(new File(INITFOLDER.toString() + "/" + lastBarcodeResult + "_" + counter + ".jpg")); break;
-                case "PARENT" : file.renameTo(new File(INITFOLDER.toString() + "/" + lastBarcodeResult  + ".jpg")); break;
+                case "CHILD" :
+                    toBeRenamed.put(file, RESFOLDER.toString() + "/" + lastBarcodeResult + "_" + counter + ".jpg");
+                    break;
+                case "PARENT" :
+                    toBeRenamed.put(file, RESFOLDER.toString() + "/" + lastBarcodeResult  + ".jpg");
+                    break;
             }
-            showFiles(++fileIndex, ++counter, lastBarcodeResult, "CHILD");
+            processFiles(++fileIndex, ++counter, lastBarcodeResult, "CHILD");
         } else {
             lastBarcodeResult = result;
-            file.renameTo(new File(INITFOLDER.toString() + "/_" + file.getName()));
-            showFiles(++fileIndex, 0, lastBarcodeResult, "PARENT");
+            executeTransaction(toBeRenamed);
+            toBeRenamed.clear();
+            toBeRenamed.put(file, RESFOLDER.toString() + "/_" + lastBarcodeResult + "_" + file.getName());
+            processFiles(++fileIndex, 0, lastBarcodeResult, "PARENT");
         }
 
     }
 
+    private static void executeTransaction(Map<File, String> map) {
+        for (Map.Entry<File, String> entry : map.entrySet()) {
+            entry.getKey().renameTo(new File(entry.getValue()));
+        }
+    }
+
     private static String getResult(File file) {
         try {
-            System.out.println(file.getName());
             if (file.getName().contains("!")) {
                 String result = file.getName().substring(1, 6);
-                System.out.println(result);
                 return result;
             }
             LuminanceSource src = new BufferedImageLuminanceSource(ImageIO.read(file.toURI().toURL()));
             BinaryBitmap imageBitmap = new BinaryBitmap(new HybridBinarizer(src));
-            Collection<Result> results = new ArrayList<>(1);
+            ArrayList<Result> results = new ArrayList<>(1);
             Reader reader = new MultiFormatReader();
 
             final Map<DecodeHintType, Object> HINTS = new EnumMap<>(DecodeHintType.class);
@@ -123,7 +132,6 @@ public class Main {
                     re.printStackTrace();
                 }
             }
-            file.renameTo(new File(INITFOLDER.toString() + "/_" + file.getName()));
 
             for (Result result : results) {
                 System.out.println(result.getText());
@@ -131,7 +139,6 @@ public class Main {
             }
 
         } catch (NotFoundException nfe) {
-            System.out.println("Could not find a barcode on the image.");
             return "Could not find a barcode on the image.";
         } catch (IOException ioe) {
             System.out.println("File reading issue.");
