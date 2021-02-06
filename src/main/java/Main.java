70e39b4bf12ecf68f1a5b8b7ef9b6c5f7f4dde89
ivanluchkin <id.luchkin@gmail.com>
2021-02-07 00:22:14 +0200

    Update .gitignore and test Windows native image on GraalVM 21.0.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
-import com.google.zxing.*;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Reader;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.multi.GenericMultipleBarcodeReader;
